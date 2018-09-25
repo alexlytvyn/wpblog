8867fe6a0caca4195a2672c128486ea043730eb2
Alexander Lytvyn <alex.lytvyn.13@gmail.com>
2018-09-26 01:47:15 +0300

    Add Main Menu.

--- functions.php
+++ functions.php
@@ -12,3 +12,20 @@ function wpblog_scripts() {
 	wp_enqueue_script('bootstrap-script', get_template_directory_uri() . '/js/bootstrap.js', array(), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'wpblog_scripts');
+
+/**
+ * Register WP Bootstrap Navwalker
+ */
+ require_once(get_template_directory() . '/inc/wp_bootstrap_navwalker.php');
+
+ /**
+  * Theme Support
+  */
+	function wpblog_theme_setup() {
+		// Nav Menus
+		register_nav_menus(array(
+			'primary' => __('Primary Menu'),
+		));
+	}
+
+	add_action('after_setup_theme', 'wpblog_theme_setup');
